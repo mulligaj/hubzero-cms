c3af93963baf96a3df2c325f693271fe4d93770b
Sam Wilson <spwilson@dev.habricentral.org>
2014-10-29 10:53:23 -0400

    [HABRICENTRAL] latest hubgraph from dev

--- components/com_hubgraph/hubgraph.php
+++ components/com_hubgraph/hubgraph.php
@@ -1,8 +1,10 @@
 <?php
-defined('JPATH_BASE') or die();
+defined('JPATH_BASE') or die(); 
+ini_set('display_errors', 1);
+require 'Inflect.php';
 
 /*
-[!] - (zooley) Added NotFoundError class because PHP was throwing
+[!] - (zooley) Added NotFoundError class because PHP was throwing 
 	fatal error that the class was not found (irony?).
 */
 if (!class_exists('NotFoundError'))
@@ -12,43 +14,43 @@ if (!class_exists('NotFoundError'))
 	}
 }
 
-$base = isset($_SERVER['SCRIPT_URL']) ? $_SERVER['SCRIPT_URL'] : $_SERVER['REDIRECT_SCRIPT_URL'];
+foreach (array('SCRIPT_URL', 'URL', 'REDIRECT_SCRIPT_URL', 'REDIRECT_URL') as $k) {
+	if (isset($_SERVER[$k])) {
+		$base = $_SERVER[$k];
+		break;
+	}
+}
 $basePath = preg_replace('#^'.preg_quote(JPATH_BASE).'#', '', dirname(__FILE__));
 
-$doc = JFactory::getDocument();
-//$doc->addScript('/jquery.js');
-\Hubzero\Document\Assets::addComponentScript('com_hubgraph','resources/hubgraph');
-\Hubzero\Document\Assets::addComponentStylesheet('com_hubgraph','resources/hubgraph.css');
 
 $path = JFactory::getApplication()->getPathway();
 $path->addItem('Search', $base);
 
-function hgView($view, $args = array())
-{
+function hgView($view, $args = array()) {
 	header('Content-type: text/json');
 	echo HubgraphClient::execView($view, array_merge($_GET, $args));
 	exit();
 }
 
-function h($str)
-{
+function p($num, $label) {
+	return Inflect::pluralizeIf($num, $label);
+}
+
+function h($str) {
 	return htmlentities($str);
 }
 
-function a($str)
-{
+function a($str) {
 	return str_replace('"', '&quot;', $str);
 }
 
-function assertSuperAdmin()
-{
+function assertSuperAdmin() {
 	if (JFactory::getUser()->usertype != 'Super Administrator') {
 		JError::raiseError(405, 'Forbidden');
 	}
 }
 
-function createNonce()
-{
+function createNonce() {
 //	set_include_path(get_include_path() . PATH_SEPARATOR . JPATH_BASE.'/libraries/openid');
 //	require_once 'Auth/OpenID/Nonce.php';
 	$now = time();
@@ -57,8 +59,7 @@ function createNonce()
 	return $_SESSION['hg_nonce'];
 }
 
-function consumeNonce($form)
-{
+function consumeNonce($form) {
 	$now = time();
 	if (!isset($form['nonce']) || $form['nonce'] != $_SESSION['hg_nonce']
 		|| !preg_match('/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\dZ/', $form['nonce'], $ma)
@@ -79,23 +80,37 @@ $conf = HubgraphConfiguration::instance();
 $perPage = 40;
 
 try {
-	switch (!defined('HG_INLINE') && isset($_REQUEST['task']) ? $_REQUEST['task'] : 'index') {
+	switch ($task = !defined('HG_INLINE') && isset($_REQUEST['task']) ? $_REQUEST['task'] : 'index') {
 		case 'complete':
 			hgView('complete', array('limit' => 20, 'threshold' => 3, 'tagLimit' => 100));
 		case 'getRelated':
 			hgView('related', $req->getTransportCriteria(array('limit' => 5, 'domain' => $_GET['domain'], 'id' => $_GET['id'])));
-		case 'index':
-			$results = $req->anyCriteria()
-				? json_decode(HubgraphClient::execView('search', $req->getTransportCriteria(array('limit' => $perPage))), TRUE)
+		case 'index': case 'page':
+			$results = $req->anyCriteria() 
+				? json_decode(HubgraphClient::execView('search', $req->getTransportCriteria(array('limit' => $perPage))), TRUE) 
 				: NULL;
-			require 'views/index.html.php';
+			$tags         = $req->getTags();
+			$users        = $req->getContributors();
+			$groups       = $req->getGroup();
+			$domainMap    = $req->getDomainMap();
+			$loggedIn     = (bool)JFactory::getUser()->id;
+			ksort($domainMap);
+			
+			if ($task == 'page') {
+				define('HG_AJAX', 1);
+				require 'views/page.html.php';
+				exit();
+			}
+			else {
+				require 'views/index-update.html.php';
+			}
 		break;
 		case 'update':
-			$results = $req->anyCriteria()
-				? json_decode(HubgraphClient::execView('search', $req->getTransportCriteria(array('limit' => $perPage))), TRUE)
+			$results = $req->anyCriteria() 
+				? json_decode(HubgraphClient::execView('search', $req->getTransportCriteria(array('limit' => $perPage))), TRUE) 
 				: NULL;
 			define('HG_AJAX', 1);
-			require 'views/index.html.php';
+			require 'views/index-update.html.php';
 			exit();
 		break;
 		case 'settings':
@@ -107,15 +122,18 @@ try {
 			consumeNonce($_POST);
 			$conf->bind($_POST)->save();
 			header('Location: /hubgraph?task=settings');
-			exit();
+			exit();	
 		break;
 		default:
 			throw new NotFoundError('no such task');
 	}
 }
 catch (Exception $ex) {
+	echo $ex->getMessage();
+	exit();
+	error_log($ex->getMessage());
 	if (!defined('HG_INLINE')) {
-		header('Location: '.JRoute::_('index.php?option=com_ysearch' . (isset($_GET['terms']) ? '&terms='.$_GET['terms'] : '')));
+		header('Location: '.JRoute::_('index.php?option=com_search' . (isset($_GET['terms']) ? '&terms='.$_GET['terms'] : '')));
 		exit();
 	}
 }
